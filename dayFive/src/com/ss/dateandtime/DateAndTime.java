33b0f95bdebb1541fb08f31ee7a5c0ac71cb0fd0
AmartHuman <55745963+AmartHuman@users.noreply.github.com>
2019-10-01 15:45:53 -0400

    Day five date and time update

--- dayFive/src/com/ss/dateandtime/DateAndTime.java
+++ dayFive/src/com/ss/dateandtime/DateAndTime.java
@@ -4,5 +4,23 @@ import java.time.*;
 
 
 public class DateAndTime {
-
+	
+	public static void main(String[] args) {
+	LocalDateTime currentTime = LocalDateTime.now();
+	
+	Month month = currentTime.getMonth();
+	int year = currentTime.getYear();
+	
+	System.out.println(year);
+	
+	for(int x = 1;x <= 12;x++) {
+		LocalDate date = LocalDate.of(year, month.of(x),1);
+		System.out.println(date.getMonth()+" : "+ date.lengthOfMonth());
+		
+		date.withDayOfMonth(2);
+	}
+	
+	
+	
+	}
 }
